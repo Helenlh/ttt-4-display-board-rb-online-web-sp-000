a6e78925ca8ca4cfda9aff57b0288a1636421a3c
HELEN HARMAN <hlharman@bellsouth.net>
2020-10-20 03:38:49 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -8,7 +8,7 @@ def display_board(board)
   puts "-----------"
   puts " #{board[6]} | #{board[7]} | #{board[8]}"
 
-
+ 
 
 board = [" ", " ", " ", " X ", " ", " ", " ", " ", " "]
   puts " #{board[0]} | #{board[1]} | #{board[2]}"
